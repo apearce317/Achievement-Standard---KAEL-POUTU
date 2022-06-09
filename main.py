24dea0fcfc5aba630794f7a377604548da986af2
apearce317 <106353001+apearce317@users.noreply.github.com>
2022-06-09 21:16:58 +0000

    Comments were changed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 score = 0
-# Question 1
+# This is Question 1
 question1 = input("How do you say Battery in Maori \n a. Puhiko \n b. Takiputa \n c. Patuhi \n d. Tiriata \n Answer: ").lower()
 
 if question1 == "a" or question1 == "puhiko":
@@ -12,7 +12,7 @@ else:
   print("Score: ", score)
   print("\n")
 
-# Question 2
+# This is Question 2
 question2 = input("How do you say Wifi in Te Reo? \n a. Waea Atamai \n b. Kupuhana \n c. Tuafafine \n d. Ahokore \n Answer: ").lower()
 
 if question2 == "d" or question2 == "ahokore":
@@ -25,7 +25,7 @@ else:
   print("Score: ", score)
   print("\n")
 
-# Question 3
+# This is Question 3
 question3 = input("How do you say Podcast in Te Reo? \n a. Pawhiri \n b. Pahorangi \n c. Nga Tautuhinga \n d. Kiriahua \n Answer: ").lower()
 
 if question3 == "b" or question3 == "pahorangi":
@@ -38,7 +38,7 @@ else:
   print("Score: ", score)
   print("\n")
 
-# Question 4 
+# This is Question 4 
 question4 = input("How do you say Folder in Te Reo? \n a. Paeahua \n b. Kopaki \n c. Rakau Pumahara \n d. Rorohiko \n Answer: ").lower()
 
 if question4 == "b" or question4 == "kopaki":
@@ -51,7 +51,7 @@ else:
   print("Score: ", score)
   print("\n")
 
-# Question 5
+# This is Question 5
 question5 = input("How do you say Text in Te Reo? \n a. Patuhi \n b. Kupuhana \n c. Tikiake \n d. Tiaki \n Answer: ").lower()
 
 if question5 == "a" or question5 == "patuhi":
@@ -65,7 +65,7 @@ else:
   print("\n")
 
 
-# final message/overall score
+# This shows your overall score and displays a message that tells you how you did
 if score <= 10:
   print("Your total score is: ", score, "- oof... better luck next time. Try again?")
 elif score <= 20:
