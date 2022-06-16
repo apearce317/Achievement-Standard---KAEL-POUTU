e9bee2b060e053d30fa3b577b5b4959438d1bb5d
apearce317 <106353001+apearce317@users.noreply.github.com>
2022-06-17 10:41:19 +1200

    Update 03_Instructions_v1.py

--- 03_Instructions_v1.py
+++ 03_Instructions_v1.py
@@ -1,2 +1,32 @@
-# Achievement-Standard---KAEL-POUTU
-Achievemnet Standard
+# Functions go here
+def yes_no(question):
+  valid = False
+  while not valid:
+    response = input(question).lower()
+    
+    if response == "yes" or response =="y":
+      response = "yes"
+      return response
+      
+    elif response == "no" or response == "n":
+      response = "no"
+      return response
+    
+    else:
+      print("Please answer yes / no")
+
+ 
+# Main Routine goes here
+def instructions():
+  print("**** How to Play ****")
+  print()
+  print("The rules of the game go here")
+  print()
+  return""
+
+played_before = yes_no("Have you played this game before? ")
+
+if played_before == "no":
+  instructions()
+  
+print("Program Continues")
