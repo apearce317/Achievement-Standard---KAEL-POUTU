eec9c00ca4028927e5da4ff9080fd48ada0b8d2d
apearce317 <106353001+apearce317@users.noreply.github.com>
2022-06-17 10:40:26 +1200

    Update 01_yes_no_v2.py

--- 01_yes_no_v2.py
+++ 01_yes_no_v2.py
@@ -1,1 +1,13 @@
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+# Ask the user if they have played before
+  show_instructions = input("Have you played this game before? ").lower()
 
+  if show_instructions == "yes" or show_instructions =="y":
+    print("program continues")
+
+  elif show_instructions == "no" or show_instructions == "n":
+    print("display instructions")
+
+  else:
+    print("Please answer yes / no")
