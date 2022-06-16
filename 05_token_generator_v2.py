1e3bf281ca9b546683b183294192f6a4934cf588
apearce317 <106353001+apearce317@users.noreply.github.com>
2022-06-17 10:46:20 +1200

    Update 05_token_generator_v2.py

--- 05_token_generator_v2.py
+++ 05_token_generator_v2.py
@@ -1,2 +1,21 @@
-# Achievement-Standard---KAEL-POUTU
-Achievemnet Standard
+import random
+
+# main routine goes here
+
+tokens = ["unicorn", "horse", "zebra", "donkey"]
+balance = 100
+
+# testing loop to generate 20 tokens
+for item in range (0,20):
+  chosen = random.choice(tokens)
+
+  # adjust balance
+  if chosen == "unicorn":
+    balance += 4
+  elif chosen == "donkey":
+    balance -= 1
+  else:
+    balance -= 0.5
+
+  # output
+  print("Token: {} , Balance: ${}".format(chosen, balance))
